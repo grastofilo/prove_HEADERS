1ebe32616ccb77af8e96c3a4f1d539d66252472c
grastofilo <mail@lucaloiacono.com>
2025-02-19 12:15:23 +0100

    Update prova_HEADERS.py

--- prova_HEADERS.py
+++ prova_HEADERS.py
@@ -4,5 +4,7 @@ import requests
 session = requests.Session()
 
 # Esegui una richiesta GET a httpbin.org per vedere quali header vengono inviati
-response = session.get("https://httpbin.org/headers")
-print("Header inviati (default):", response.json())
+if st.button('premi'):
+  response = session.get("https://httpbin.org/headers")
+  st.write(response.json())
+  print("Header inviati (default):", response.json())
